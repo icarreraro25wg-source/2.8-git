c588e31be80cef2b9d69894fae992d737de32835
ik012982i12 <icarreraro25wg@ikzubirimanteo.com>
2025-11-18 10:31:59 +0100

    faktorialak

--- src/calculator.java
+++ src/calculator.java
@@ -1,7 +1,7 @@
 class calculator {
     long faktoriala(int n) {
         if (n < 0) throw new IllegalArgumentException("Zenbaki negatiboak ez dira onartzen");
-        long emaitza = 1;
+        long emaitza = 1; 
         for (int i = 1; i <= n; i++) {
             emaitza *= i;
         }
